417bd3708628873565e474d2f6454d2494989b67
unknown <g.anas786@gmail.com>
2023-03-03 01:34:36 +0530

    modified put and delete method

--- routes/genres.js
+++ routes/genres.js
@@ -26,35 +26,33 @@ router.post('/api/genres', async (req, res) => {
     const { error } = req.body;
     if (error) return res.send(400).send(error.details[0].message);
 
-    let genre = new Genre({name: req.body.name})
+    let genre = new Genre({ name: req.body.name })
     genre = await genre.save()
-    
+
     res.send(genre);
 })
 
 router.put('/api/genres/:id', async (req, res) => {
-    const {error } = req.body.name
+    const { error } = req.body.name
     if (error) return res.status(400).send(error.details[0].message)
-    
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+
+    const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name }, {
+        new: true
+    });
     if (!genre) return res.status(404).send('The genre with the given ID was not found')
 
-    // const { error } = req.body;
     if (error) return res.status(400).send({ error: error.message });
 
-    genre.name = req.body.name;
     res.send(genre);
 })
 
-router.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
+router.delete('/api/genres/:id', async (req, res) => {
+    const genre = await Genre.findByIdAndRemove(req.params.id);
+
     if (!genre) return res.status(404).send('The genre with the given ID was not found.');
 
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1)
-
     res.send(genre);
-})
+});
 
 router.get('/api/genres/:id', (req, res) => {
     const genre = genres.find(c => c.id === parseInt(req.params.id));
