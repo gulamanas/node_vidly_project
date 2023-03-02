9d3788aa3696812d005646d7ee899c256469dd19
unknown <g.anas786@gmail.com>
2023-03-03 01:42:00 +0530

    fixed error

--- routes/genres.js
+++ routes/genres.js
@@ -54,12 +54,6 @@ router.delete('/api/genres/:id', async (req, res) => {
     res.send(genre);
 });
 
-router.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id === parseInt(req.params.id));
-    if (!genre) return res.status(404).send('The genre with the given ID was not found.');
-    res.send(genre);
-});
-
 router.get('/api/genres/:id', async (req, res) => {
     const genre = await Genre.findById(req.params.id)
 
