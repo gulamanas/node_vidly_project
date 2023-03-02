b77d72250afc16641521cb16f1dc214a4d381e9b
unknown <g.anas786@gmail.com>
2023-03-03 01:59:10 +0530

    put delete and get single element for customer api

--- routes/customers.js
+++ routes/customers.js
@@ -42,4 +42,37 @@ router.post('/api/customers', async (req, res) => {
     res.send(customer);
 })
 
+router.put('/api/customers/:id', async (req, res) => {
+    const { error } = req.body;
+    if (error) return res.status(400).send(error.details[0].message);
+
+    const customer = await Customer.findByIdAndUpdate(req.params.id, {
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone,
+    }, {
+        new: true
+    });
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+    res.send(customer);
+});
+
+router.delete('/api/customers/:id', async (req, res) => {
+    const customer = await Customer.findByIdAndRemove(req.params.id);
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+    res.send(customer);
+})
+
+router.get('/api/customers/:id', async (req, res) => {
+    const customer = await Customer.findById(req.params.id)
+
+    if (!customer) return res.status(404).send('The customer with the given ID was not found.');
+
+    res.send(customer);
+});
+
 module.exports = router;
