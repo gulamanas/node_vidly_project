ac3b5e30840e9263e7d3b7e5384e375642a7474f
unknown <g.anas786@gmail.com>
2023-03-08 01:09:53 +0530

    jwt token

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,4 @@
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 const { User } = require('../models/user');
 const express = require('express');
@@ -12,7 +13,8 @@ router.post('/api/login', async (req, res) => {
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword) return res.status(400).send('Invalid password');
 
-        res.send(true)
+        const token = jwt.sign({ _id: user._id }, 'jwtPrivateKey')
+        res.send(token)
     } catch (error) {
         res.status(500).send(`${err.message}`)
     }
